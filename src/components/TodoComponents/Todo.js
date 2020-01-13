71ef9748957ded5146296f8d5ff8e7c6891b9556
mlazzell17@gmail.com <MicheSi>
2020-01-13 13:27:09 -0700

    started building TodoList and Todo components

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const todo = [
+    {
+        task: 'Wash laundry',
+        id: 1,
+        completed: false
+    },
+    {
+        task: 'Clean the house',
+        id: 2,
+        completed: false
+    }
+]
+
+export default todo;
